3ba4dc93e5651ffa98625b577ac678c069b90afb
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2024-01-18 16:55:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-function makeid(l) {
+function makeid(length) {
   // write your code here
+	 const alphanumericCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomString = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * alphanumericCharacters.length);
+    randomString += alphanumericCharacters.charAt(randomIndex)
+  }
+
+  return randomString;
 }
 
 // Do not change the code below.
